7457b5fb0a2a233c2e9150ffe9fac3c0d3e3493b
Cem Ozturan <cemozturan@gmail.com>
2020-09-15 10:14:10 +0100

    reverting syntax experiment

--- rules/add-customer-id-to-access-token.js
+++ rules/add-customer-id-to-access-token.js
@@ -1,6 +1,6 @@
 function (user, context, callback) {
   const namespace = "http://pret/customerId";
-  if (user?.app_metadata?.pret_customer_id && context?.accessToken) {
+  if (user && user.app_metadata && user.app_metadata.pret_customer_id && context.accessToken) {
     context.accessToken[namespace] = user.app_metadata.pret_customer_id;
   }
   return callback(null, user, context);
