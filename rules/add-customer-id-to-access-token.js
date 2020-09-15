a52bdb4890fc90c79e2af90fd89aade07e317284
Cem Ozturan <cemozturan@gmail.com>
2020-09-15 10:40:05 +0100

    experimenting with export

--- rules/add-customer-id-to-access-token.js
+++ rules/add-customer-id-to-access-token.js
@@ -1,5 +1,5 @@
-function addCustomerIdToAccessToken(user, context, callback) {
-  const namespace = "http://pret/customerId";
+export function addCustomerIdToAccessToken(user, context, callback) {
+  const namespace = "http://pret/customerIddd";
   if (user && user.app_metadata && user.app_metadata.pret_customer_id && context && context.accessToken) {
     context.accessToken[namespace] = user.app_metadata.pret_customer_id;
   }
