720a02654c9a359cf0084107ef1134eac3d5903e
Cem Ozturan <cemozturan@gmail.com>
2020-09-15 10:46:16 +0100

    trying a self-executing anon function

--- rules/add-customer-id-to-access-token.js
+++ rules/add-customer-id-to-access-token.js
@@ -1,7 +1,16 @@
-function addCustomerIdToAccessToken(user, context, callback) {
-  const namespace = "http://pret/customerIddd";
-  if (user && user.app_metadata && user.app_metadata.pret_customer_id && context && context.accessToken) {
-    context.accessToken[namespace] = user.app_metadata.pret_customer_id;
+(()=>{
+  function (user, context, callback) {
+    const namespace = "http://pret/customerIdd";
+    if (user && user.app_metadata && user.app_metadata.pret_customer_id && context && context.accessToken) {
+      context.accessToken[namespace] = user.app_metadata.pret_customer_id;
+    }
+    return callback(null, user, context);
   }
-  return callback(null, user, context);
-}
+   
+  if (module) {
+    module.exports = rule;
+  }
+   
+  return rule;
+   
+})()
